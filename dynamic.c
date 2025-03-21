7d830b9bf1451e5e5d6eacc8fb9e719a7c6ab44c
Bran Quanne <branmq@gmail.com>
2025-03-21 10:22:39 +0100

    ändringar

--- dynamic.c
+++ dynamic.c
@@ -1,11 +1,64 @@
 #include <stdio.h>
 
-int main(void) {
-  int burger1, burger2, max_time;
+int dynamic(int time_to_eat_burger_1, int time_to_eat_burger_2, int *total_time);
 
-  while (scanf("%d %d %d", &burger1, &burger2, &max_time) != EOF) {
+int main(void)
+{
+
+  int time_to_eat_burger_1, time_to_eat_burger_2, total_time, burgers_eaten;
+
+  while (scanf("%d %d %d", &time_to_eat_burger_1, &time_to_eat_burger_2, &total_time) != EOF)
+  {
+
+    burgers_eaten = dynamic(time_to_eat_burger_1, time_to_eat_burger_2, &total_time);
+
+    if (total_time > 0)
+    {
+      printf("%d %d\n", burgers_eaten, total_time);
+    }
+    else
+    {
+      printf("%d\n", burgers_eaten);
+    }
   }
 
-  int burgers_eaten = 0;
-  int time_drinking = 0;
+  return 0;
+}
+
+int dynamic(int time_to_eat_burger_1, int time_to_eat_burger_2, int *total_time)
+{
+  int arr[10000];
+  int tot_burgers = 0;
+  arr[0] = tot_burgers;
+
+  for (int i = 1; *total_time >= time_to_eat_burger_2 || *total_time >= time_to_eat_burger_1; i++)
+  {
+    if ((*total_time % time_to_eat_burger_2) < (*total_time % time_to_eat_burger_1))
+    {
+      arr[i] = arr[i - 1] + 1;
+      *total_time -= time_to_eat_burger_2;
+    }
+
+    else if ((*total_time % time_to_eat_burger_1) < (*total_time % time_to_eat_burger_2))
+    {
+      arr[i] = arr[i - 1] + 1;
+      *total_time -= time_to_eat_burger_1;
+    }
+
+    else
+    {
+      if (time_to_eat_burger_2 < time_to_eat_burger_1)
+      {
+        arr[i] = arr[i - 1] + 1;
+        *total_time -= time_to_eat_burger_2;
+      }
+      else
+      {
+        arr[i] = arr[i - 1] + 1;
+        *total_time -= time_to_eat_burger_1;
+      }
+    }
+    tot_burgers = arr[i];
+  }
+  return tot_burgers;
 }
--- memoization.c
+++ memoization.c
@@ -1,17 +1,84 @@
 #include <stdio.h>
 
-int memoization(int n, int m, int t, int *time_drinking);
+struct memo_table
+{
+  int burgers_eaten;
+  int time_left;
+};
 
-int main(void) {
-  int n, m, t, num_burgers, time_drinking = 0;
+int memoization(int time_to_eat_burger_1, int time_to_eat_burger_2, int time_left, struct memo_table mtable[]);
 
-  while (scanf("%d %d %d", &n, &m, &t) != EOF) {
+int main(void)
+{
+
+  int time_to_eat_burger_1, time_to_eat_burger_2, total_time;
+
+  while (scanf("%d %d %d", &time_to_eat_burger_1, &time_to_eat_burger_2, &total_time) != EOF)
+  {
+
+    struct memo_table memo_table[total_time + 1];
+
+    for (int i = 0; i <= total_time; i++)
+    {
+      memo_table[i].burgers_eaten = -1;
+      memo_table[i].time_left = -1;
+    }
+
+    memoization(time_to_eat_burger_1, time_to_eat_burger_2, total_time, memo_table);
+
+    if (memo_table[total_time].time_left > 0)
+    {
+      printf("%d %d\n", memo_table[total_time].burgers_eaten, memo_table[total_time].time_left);
+    }
+    else
+    {
+      printf("%d\n", memo_table[total_time].burgers_eaten);
+    }
   }
 
   return 0;
 }
 
-int memoization(int n, int m, int t, int *time_drinking) {
+int memoization(int time_to_eat_burger_1, int time_to_eat_burger_2, int time_left, struct memo_table mtable[])
+{
 
-  return 0;
+  if (time_left < time_to_eat_burger_1 && time_left < time_to_eat_burger_2)
+  {
+    mtable[time_left].time_left = time_left;
+    mtable[time_left].burgers_eaten = 0;
+    return 0;
+  }
+
+  if (mtable[time_left].burgers_eaten != -1)
+  {
+    return mtable[time_left].burgers_eaten;
+  }
+
+  int remaining_time_1 = time_left, remaining_time_2 = time_left;
+  int result_burger_1 = 0, result_burger_2 = 0;
+
+  if (time_left >= time_to_eat_burger_1)
+  {
+    result_burger_1 = 1 + memoization(time_to_eat_burger_1, time_to_eat_burger_2, time_left - time_to_eat_burger_1, mtable);
+    remaining_time_1 = mtable[time_left - time_to_eat_burger_1].time_left;
+  }
+
+  if (time_left >= time_to_eat_burger_2)
+  {
+    result_burger_2 = 1 + memoization(time_to_eat_burger_1, time_to_eat_burger_2, time_left - time_to_eat_burger_2, mtable);
+    remaining_time_2 = mtable[time_left - time_to_eat_burger_2].time_left;
+  }
+
+  if (remaining_time_1 < remaining_time_2 || (remaining_time_1 == remaining_time_2 && result_burger_1 > result_burger_2))
+  {
+    mtable[time_left].time_left = remaining_time_1;
+    mtable[time_left].burgers_eaten = result_burger_1;
+  }
+  else
+  {
+    mtable[time_left].time_left = remaining_time_2;
+    mtable[time_left].burgers_eaten = result_burger_2;
+  }
+
+  return mtable[time_left].burgers_eaten;
 }
