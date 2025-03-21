f5dc0871187482520f1fa6c45c5d1a8cdb048a5c
Bran Quanne <branmq@gmail.com>
2025-03-21 13:48:47 +0100

    wallah

--- memoization.c
+++ memoization.c
@@ -1,69 +1,69 @@
 #include <stdio.h>
 
 struct memo_table {
-  int burgers_eaten;
-  int time_left;
+    int burgers_eaten;
+    int time_left;
 };
 
 int memoization(int n, int m, int time_left, struct memo_table mtable[]);
 
 int main(void) {
 
-  int n, m, t;
+    int n, m, t;
 
-  while (scanf("%d %d %d", &n, &m, &t) != EOF) {
+    while (scanf("%d %d %d", &n, &m, &t) != EOF) {
 
-    struct memo_table memo_table[t + 1];
+        struct memo_table memo_table[t + 1];
 
-    for (int i = 0; i <= t; i++) {
-      memo_table[i].burgers_eaten = -1;
-      memo_table[i].time_left = -1;
+        for (int i = 0; i <= t; i++) {
+            memo_table[i].burgers_eaten = -1;
+            memo_table[i].time_left = -1;
+        }
+
+        memoization(n, m, t, memo_table);
+
+        if (memo_table[t].time_left > 0) {
+            printf("%d %d\n", memo_table[t].burgers_eaten, memo_table[t].time_left);
+        } else {
+            printf("%d\n", memo_table[t].burgers_eaten);
+        }
     }
 
-    memoization(n, m, t, memo_table);
-
-    if (memo_table[t].time_left > 0) {
-      printf("%d %d\n", memo_table[t].burgers_eaten, memo_table[t].time_left);
-    } else {
-      printf("%d\n", memo_table[t].burgers_eaten);
-    }
-  }
-
-  return 0;
+    return 0;
 }
 
 int memoization(int n, int m, int time_left, struct memo_table mtable[]) {
 
-  if (time_left < n && time_left < m) {
-    mtable[time_left].time_left = time_left;
-    mtable[time_left].burgers_eaten = 0;
-    return 0;
-  }
+    if (time_left < n && time_left < m) {
+        mtable[time_left].time_left = time_left;
+        mtable[time_left].burgers_eaten = 0;
+        return 0;
+    }
 
-  if (mtable[time_left].burgers_eaten != -1) {
+    if (mtable[time_left].burgers_eaten != -1) {
+        return mtable[time_left].burgers_eaten;
+    }
+
+    int remaining_time_1 = time_left, remaining_time_2 = time_left;
+    int result_burger_1 = 0, result_burger_2 = 0;
+
+    if (time_left >= n) {
+        result_burger_1 = 1 + memoization(n, m, time_left - n, mtable);
+        remaining_time_1 = mtable[time_left - n].time_left;
+    }
+
+    if (time_left >= m) {
+        result_burger_2 = 1 + memoization(n, m, time_left - m, mtable);
+        remaining_time_2 = mtable[time_left - m].time_left;
+    }
+
+    if (remaining_time_1 < remaining_time_2 || (remaining_time_1 == remaining_time_2 && result_burger_1 > result_burger_2)) {
+        mtable[time_left].time_left = remaining_time_1;
+        mtable[time_left].burgers_eaten = result_burger_1;
+    } else {
+        mtable[time_left].time_left = remaining_time_2;
+        mtable[time_left].burgers_eaten = result_burger_2;
+    }
+
     return mtable[time_left].burgers_eaten;
-  }
-
-  int remaining_time_1 = time_left, remaining_time_2 = time_left;
-  int result_burger_1 = 0, result_burger_2 = 0;
-
-  if (time_left >= n) {
-    result_burger_1 = 1 + memoization(n, m, time_left - n, mtable);
-    remaining_time_1 = mtable[time_left - n].time_left;
-  }
-
-  if (time_left >= m) {
-    result_burger_2 = 1 + memoization(n, m, time_left - m, mtable);
-    remaining_time_2 = mtable[time_left - m].time_left;
-  }
-
-  if (remaining_time_1 < remaining_time_2 || (remaining_time_1 == remaining_time_2 && result_burger_1 > result_burger_2)) {
-    mtable[time_left].time_left = remaining_time_1;
-    mtable[time_left].burgers_eaten = result_burger_1;
-  } else {
-    mtable[time_left].time_left = remaining_time_2;
-    mtable[time_left].burgers_eaten = result_burger_2;
-  }
-
-  return mtable[time_left].burgers_eaten;
 }
--- recursion.c
+++ recursion.c
@@ -3,53 +3,53 @@
 int simple_recursion(int m, int n, int t, int *time_drinking);
 
 int main(void) {
-  int n, m, t;
-  int num_burgers;
-  int time_drinking = 0;
+    int n, m, t;
+    int num_burgers;
+    int time_drinking = 0;
 
-  while (scanf("%d %d %d", &n, &m, &t) != EOF) {
-    num_burgers = simple_recursion(m, n, t, &time_drinking);
+    while (scanf("%d %d %d", &n, &m, &t) != EOF) {
+        num_burgers = simple_recursion(m, n, t, &time_drinking);
 
-    if (time_drinking != 0) {
-      printf("%d %d\n", num_burgers, time_drinking);
-    } else {
-      printf("%d\n", num_burgers);
+        if (time_drinking != 0) {
+            printf("%d %d\n", num_burgers, time_drinking);
+        } else {
+            printf("%d\n", num_burgers);
+        }
     }
-  }
 
-  return 0;
+    return 0;
 }
 
 int simple_recursion(int m, int n, int t, int *time_drinking) {
-  if (t < 0) {
-    return 0;
-  }
-  if (t < m && t < n) {
-    *time_drinking = t;
-    return 0;
-  }
+    if (t < 0) {
+        return 0;
+    }
+    if (t < m && t < n) {
+        *time_drinking = t;
+        return 0;
+    }
 
-  int time_m = t;
-  int time_n = t;
+    int time_m = t;
+    int time_n = t;
 
-  int max_m = simple_recursion(m, n, t - m, &time_m) + 1;
-  int max_n = simple_recursion(m, n, t - n, &time_n) + 1;
+    int max_m = simple_recursion(m, n, t - m, &time_m) + 1;
+    int max_n = simple_recursion(m, n, t - n, &time_n) + 1;
 
-  if (max_m == max_n) {
-    *time_drinking = (time_m < time_n) ? time_m : time_n;
-    return max_m;
-  }
+    if (max_m == max_n) {
+        *time_drinking = (time_m < time_n) ? time_m : time_n;
+        return max_m;
+    }
 
-  if (time_m == time_n) {
-    *time_drinking = time_m;
-    return (max_m > max_n) ? max_m : max_n;
-  }
+    if (time_m == time_n) {
+        *time_drinking = time_m;
+        return (max_m > max_n) ? max_m : max_n;
+    }
 
-  if (time_m < time_n) {
-    *time_drinking = time_m;
-    return max_m;
-  }
+    if (time_m < time_n) {
+        *time_drinking = time_m;
+        return max_m;
+    }
 
-  *time_drinking = time_n;
-  return max_n;
+    *time_drinking = time_n;
+    return max_n;
 }
